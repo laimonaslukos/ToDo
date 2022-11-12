b4c40f52c138aa06c1f2a542daaf03954d88f69f
„Laimonas <laimonas.lukosevicius@gmail.com>
2022-11-12 20:29:33 +0200

    add function for adding tasks

--- scripts.js
+++ scripts.js
@@ -8,11 +8,32 @@ const tasks = [
 
 
 function addNewTask(){
-document.getElementById('task-input');
+const taskInput = document.getElementById('task-input');
+console.log('taskInput', taskInput.value);
+
+const ul = document.querySelector('.todo-list-ul');
+const li = document.createElement('li');
+  li.classList.add('list-group-item');
+li.onclick = () => {
+  li.classList.add('active')
+}
+
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-success');
+  button.innerText = 'DELETE';
+  button.onclick = () =>{
+li.remove();
+
+  }
+
+
+  li.append(button, taskInput.value);
+  ul.append(li)
 
 }
 
-
+const btn = document.querySelector('.btn-input');
+btn.addEventListener('click', addNewTask)
 
 function getTodoList(alltasks) {
   const ul = document.querySelector('.todo-list-ul');
