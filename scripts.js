1dd4f2dc430f3d5b5eaa7f53fc2b054f7e6cb503
„Laimonas <laimonas.lukosevicius@gmail.com>
2022-11-12 20:01:19 +0200

    add function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,30 @@
-const task = [
-{task: "go to work"},
-{task: "go to the gym"},
-{task: "shoping"},
-{task: "do me"}
+const tasks = [
+{task: 'go to work'},
+{task: 'go to the gym'},
+{task: 'shoping'},
+{task: 'do me'},
 
 ];
-console.log("scripts working");
+console.log('scripts working');
+
+
+
+function getTodoList(alltasks) {
+  const ul = document.querySelector('.todo-list-ul');
+const liElements = alltasks.map((elem)=> {
+
+  const li = document.createElement('li');
+  li.classList.add('list-group-item');
+  li.append(elem.task);
+  console.log('li', li);
+  return li
+
+
+
+
+})  
+
+  return ul.append(...liElements);
+}
+
+getTodoList(tasks);
